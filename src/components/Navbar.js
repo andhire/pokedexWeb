c6fe864c171bbc23c5f9598547e0bc60a179c7c7
Andres Higuera <higuera_andres@hotmail.com>
2019-06-22 23:22:05 -0500

    Se mejoro navegacion de navbar y diseño.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,54 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import "./styles/card.css";
 function Navbara(props) {
     return (
         <React.Fragment>
-            <Navbar bg="danger" variant="dark">
-                <NavLink to="/" className="not-active-Pokedex">
-                    <img
-                        src="http://rs795.pbsrc.com/albums/yy232/PixKaruumi/Pixels/Pixels%2081/1Pikachu_Moving_Sprite.gif~c200"
-                        height="40"
-                        width="40"
-                        style={{ paddingBottom: 10 + "px" }}
-                        alt="Pikachu gif"
-                    />
-                    Pokedex
-                </NavLink>
+            <Navbar bg="danger" expand="lg" variant="dark">
+                <LinkContainer to="/">
+                    <Navbar.Brand>
+                        <img
+                            src="http://rs795.pbsrc.com/albums/yy232/PixKaruumi/Pixels/Pixels%2081/1Pikachu_Moving_Sprite.gif~c200"
+                            height="40"
+                            width="40"
+                            style={{ paddingBottom: 10 + "px" }}
+                            alt="Pikachu gif"
+                        />
+                        Pokedex
+                    </Navbar.Brand>
+                </LinkContainer>
 
-                <Navbar.Collapse id="responsive-navbar-nav">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto" />
                     <Nav>
-                        <NavLink to="/Kanto/" className="not-active">
-                            1st Gen. (Kanto)
-                        </NavLink>
-                        <NavLink to="/johto/" className="not-active">
-                            2nd Gen. (Johto)
-                        </NavLink>
-                        <NavLink to="/hoenn/" className="not-active">
-                            3rd Gen. (Hoenn)
-                        </NavLink>
-                        <NavLink to="/sinnoh/" className="not-active">
-                            4th Gen. (Sinnoh)
-                        </NavLink>
-                        <NavLink to="/teselia/" className="not-active">
-                            5th Gen. (Teselia)
-                        </NavLink>
-                        <NavLink to="/kalos/" className="not-active">
-                            6th Gen. (Kalos)
-                        </NavLink>
-                        <NavLink to="/alola/" className="not-active">
-                            7th Gen. (Alola)
-                        </NavLink>
+                        <LinkContainer to="/Kanto/">
+                            <Nav.Link>1st Gen. (Kanto)</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to="/johto/">
+                            <Nav.Link>2nd Gen. (Johto)</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/hoenn/">
+                            <Nav.Link>3rd Gen. (Hoenn)</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/sinnoh/">
+                            <Nav.Link>4th Gen. (Sinnoh)</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to="/teselia/">
+                            <Nav.Link>5th Gen. (Teselia)</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to="/kalos/">
+                            <Nav.Link>6th Gen. (Kalos)</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to="/alola/">
+                            <Nav.Link>7th Gen. (Alola)</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
